5ed91e0f50aff5cdc136d7385442708b11883050
Lucas <lucasalexandre_82@hotmail.com>
2023-06-02 12:43:29 -0300

    Cadastrando colaboradores.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
+import { useState } from 'react';
 import Banner from './componentes/Banner';
 import Formulario from './componentes/Formulario';
 
 function App() {
+
+  const [colaboradores, setColaboradores] = useState([]);
+
+  const colaboradorAdicionado = (colaborador) => {
+    console.log(colaborador);
+    setColaboradores([...colaboradores, colaborador]);
+  }
+
   return (
     <div className="App">
       <Banner />
-      <Formulario />
+      <Formulario colaboradorCadastrado={colaborador => colaboradorAdicionado(colaborador)} />
     </div>
   );
 }
