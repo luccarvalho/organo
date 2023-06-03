54e17b86cebf5d36591b691bd93385596d5ae4b2
Lucas <lucasalexandre_82@hotmail.com>
2023-06-03 13:04:24 -0300

    Criando o componente Time.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import Banner from './componentes/Banner';
 import Formulario from './componentes/Formulario';
+import Time from './componentes/Time';
 
 function App() {
 
@@ -15,6 +16,8 @@ function App() {
     <div className="App">
       <Banner />
       <Formulario colaboradorCadastrado={colaborador => colaboradorAdicionado(colaborador)} />
+      <Time nome="Programação" />
+      <Time nome="Frontend" />
     </div>
   );
 }
