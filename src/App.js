66d02e1e88ddc3e48256539b2750fc51003dd9c0
Lucas <lucasalexandre_82@hotmail.com>
2023-06-04 12:43:56 -0300

    Elevando a lista de times.

--- src/App.js
+++ src/App.js
@@ -5,6 +5,44 @@ import Time from './componentes/Time';
 
 function App() {
 
+  const times = [
+    {
+      nome: 'Programação',
+      corPrimaria: '#57c278',
+      corSecundaria: '#d9f7e9'
+    },
+    {
+      nome: 'Front-end',
+      corPrimaria: '#82cffa',
+      corSecundaria: '#e8f8ff'
+    },
+    {
+      nome: 'Data Science',
+      corPrimaria: '#a6d157',
+      corSecundaria: '#f0f8e2'
+    },
+    {
+      nome: 'Devops',
+      corPrimaria: '#e06b69',
+      corSecundaria: '#fde7e8'
+    },
+    {
+      nome: 'UX e Design',
+      corPrimaria: '#d86ebf',
+      corSecundaria: '#fae9f5'
+    },
+    {
+      nome: 'Mobile',
+      corPrimaria: '#ffba05',
+      corSecundaria: '#fff5d9'
+    },
+    {
+      nome: 'Inovação e Gestão',
+      corPrimaria: '#ff8a29',
+      corSecundaria: '#ffeedf'
+    }
+  ]
+
   const [colaboradores, setColaboradores] = useState([]);
 
   const colaboradorAdicionado = (colaborador) => {
@@ -16,8 +54,9 @@ function App() {
     <div className="App">
       <Banner />
       <Formulario colaboradorCadastrado={colaborador => colaboradorAdicionado(colaborador)} />
-      <Time nome="Programação" />
-      <Time nome="Frontend" />
+
+      {times.map(time => <Time key={time.nome} nome={time.nome} corPrimaria={time.corPrimaria} corSecundaria={time.corSecundaria} />)}
+
     </div>
   );
 }
--- src/componentes/Time/index.js
+++ src/componentes/Time/index.js
@@ -1,9 +1,13 @@
 import './Time.css'
 
 const Time = (props) => {
+
+    const css1 = { backgroundColor: props.corSecundaria };
+    const css2 = { borderColor: props.corPrimaria };
+
     return (
-        <section>
-            <h3>{props.nome}</h3>
+        <section className='time' style={css1}>
+            <h3 style={css2}>{props.nome}</h3>
         </section>
     )
 }
